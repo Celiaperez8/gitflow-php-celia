f9fb1a271104ad177b5a6dca594fdd9902a42148
Celiaperez8 <156211946+Celiaperez8@users.noreply.github.com>
2025-03-24 19:41:56 +0100

    Ajustes finales para la versión v1.0

--- alumnos/index.php
+++ alumnos/index.php
@@ -1,8 +1,4 @@
 <?php
-<<<<<<< HEAD
 include "alumnos/version_conflictiva.php";
-=======
-include "alumnos/version_conflictiva.php"; // contenido diferente al de develop
->>>>>>> feature/conflicto-celia
 ?>
 
